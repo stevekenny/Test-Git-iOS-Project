c7c590e73cf9eba77eec8e90a5c9e4875d6a24d3
Stephen Kenny <Stephen@madmartigan.home>
2014-03-30 00:18:28 -0400

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Stephen Kenny on 3/30/14.
+//  Copyright (c) 2014 Mystic Rhythm Studio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Stephen Kenny on 3/30/14.
+//  Copyright (c) 2014 Mystic Rhythm Studio. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
