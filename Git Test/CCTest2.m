1302edaf0519f8d18ed3230f6f8d8b83005d73dc
Stephen Kenny <stevekenny@mac.com>
2014-04-02 12:36:16 -0400

    added Test 2 class

---
+++ Git Test/CCTest2.m
@@ -0,0 +1,13 @@
+//
+//  CCTest2.m
+//  Git Test
+//
+//  Created by Stephen Kenny on 4/2/14.
+//  Copyright (c) 2014 Mystic Rhythm Studio. All rights reserved.
+//
+
+#import "CCTest2.h"
+
+@implementation CCTest2
+
+@end
